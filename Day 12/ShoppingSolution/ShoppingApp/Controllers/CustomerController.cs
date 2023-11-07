dd9c9399d6ab47d8b09260faf384e6e71b68c68d
Niranjan R <niranjanece21@gmail.com>
2023-11-07 18:11:06 +0530

    Day13

---
+++ Day 12/ShoppingSolution/ShoppingApp/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Interfaces;
+using ShoppingApp.Models.DTOs;
+
+namespace ShoppingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public CustomerController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost]
+        public ActionResult Register(UserDTO viewModel)
+        {
+            string message = "";
+            try
+            {
+                var user = _userService.Register(viewModel);
+                if(user != null)
+                {
+                    return Ok(user);
+                }
+            }
+            catch (DbUpdateException exp) {
+                message = "Duplicate username";
+            }
+            catch(Exception) { 
+                
+            }
+            return BadRequest(message);
+        }
+        [HttpGet]
+        public ActionResult Login(UserDTO viewModel) {
+            string message = "Invalid username or password";
+            var user = _userService.Login(viewModel);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return BadRequest(message);
+        }
+    }
+}
